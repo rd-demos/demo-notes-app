de0405afe275fbb5cb93e3a9828be4401dc48135
Ryan Doherty <rdoherty@aais.com>
2021-12-06 13:19:59 -0600

    adding auth into the workflow for API Security

--- stacks/ApiStack.js
+++ stacks/ApiStack.js
@@ -11,6 +11,8 @@ export default class ApiStack extends sst.Stack {
 
         // Create the API
         this.api = new sst.Api(this, "Api", {
+            defaultAuthorizationType: "AWS_IAM",
+
             defaultFunctionProps: {
                 environment: {
                     TABLE_NAME: table.tableName,
