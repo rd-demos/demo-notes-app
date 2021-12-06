fc3aecbe19f850d6716a148bd372b54c95b55be8
Ryan Doherty <rdoherty@aais.com>
2021-12-06 14:39:14 -0600

    integrated with Stripe API for billing

--- stacks/ApiStack.js
+++ stacks/ApiStack.js
@@ -16,6 +16,7 @@ export default class ApiStack extends sst.Stack {
             defaultFunctionProps: {
                 environment: {
                     TABLE_NAME: table.tableName,
+                    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
                 },
             },
             routes: {
@@ -24,6 +25,7 @@ export default class ApiStack extends sst.Stack {
                 "GET    /notes": "src/list.main",
                 "PUT    /notes/{id}": "src/update.main",
                 "DELETE /notes/{id}": "src/delete.main",
+                "POST   /billing": "src/billing.main",
             },
         });
 
